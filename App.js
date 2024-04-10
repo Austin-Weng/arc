f01730afbaa73ea42c5d28cf4b27aa6563a176f3
Austin-Weng <73495049+Austin-Weng@users.noreply.github.com>
2024-04-10 14:26:49 -0700

    Added header and Navigation Container

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <NavigationContainer >
+      <View style={styles.container}>
+        <Text style={styles.header}> Acropolis Robotics Center</Text>
+        <StatusBar style="auto" />
+      </View>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
+  );
+}
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
     </View>
   );
 }
@@ -15,6 +42,32 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+  },
+  header: {
+    textAlign: 'center',
+    width: '100%',
+    borderWidth: 5,
+    borderColor: 'black',
+    paddingTop: 30,
+    paddingBottom: 30,
+    fontSize: 50,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+  },
+  buttonBox: {
     justifyContent: 'center',
+    flexDirection: 'row',
+    width: '100%',
+    paddingTop: 10,
+    paddingBottom: 10,
+    borderColor: 'black',
+    borderBottomWidth: 5,
+    borderLeftWidth: 5,
+    borderRightWidth: 5,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  button: {
+    margin: 10,
   },
 });
